29ea9b45a6eb31e5a958a7f7ca75fa10709664df
mikepierre <mikeypierre84@yahoo.com>
2016-01-07 11:06:18 -0500

    Bridge Pattern

---
+++ bridge.py
@@ -0,0 +1,18 @@
+class DrawingAPIOne(object):
+	"""docstring for DrawingAPIOne"""
+	def __init__(self):
+		super(DrawingAPIOne, self).__init__()
+
+	def draw_cirlce(self, x, y, radius):
+		print("First dawhing api with cicle at ({}, {} with a radius of {})".format(x, y, radius))
+
+class DrawingAPITwo(object):
+	"""docstring for DrawingAPITwo"""
+	def __init__(self, arg):
+		super(DrawingAPITwo, self).__init__()
+
+	def draw_circle(self, x, y, radius):
+		print("First dawhing api with cicle at ({}, {} with a radius of {})".format(x, y, radius))
+		
+		
+		
