4856b45b43214b128c34fc9b5e026cae6e8370ed
mikepierre <mikeypierre84@yahoo.com>
2016-01-07 12:51:53 -0500

    Bridge Pattern

--- bridge.py
+++ bridge.py
@@ -13,6 +13,24 @@ class DrawingAPITwo(object):
 
 	def draw_circle(self, x, y, radius):
 		print("First dawhing api with cicle at ({}, {} with a radius of {})".format(x, y, radius))
-		
-		
-		
+
+class Circle(object):
+	"""docstring for Circle"""
+	def __init__(self, x, y, radius, drawing_api):
+		super(Circle, self).__init__()
+		self._x = x
+		self._y = y
+		self._radius = radius
+		self._drawing_api = drawing_api
+
+	def draw(self):
+		self._drawing_api.draw_circle(self._x, self._y, self._radius)
+
+	def scale(self, percent):
+		self._radius *= percent
+
+circle1 = Circle(1, 2, 3, DrawingAPIOne())
+circle1.draw()
+
+circle2 = Circle(2, 3, 4, DrawingAPITwo())
+circle2.draw()
